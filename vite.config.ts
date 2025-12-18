e83f1f77502cfbd6410dba0232d5acbbab1933e8
sivarajan2003 <sivarajan2.21msc@kongu.edu>
2025-12-18 09:18:06 +0530

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/preskool-dashboard/", // 👈 MUST MATCH REPO NAME
+  //base: "/preskool-dashboard/", // 👈 MUST MATCH REPO NAME
 });
